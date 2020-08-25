01cadecefad868c5cd23808ad00ba67cf92b981b
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-25 19:07:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "m", "Dark Brown", "Kind", 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "m", "Light Brown", "Active", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "m", "Light Brown", "Active", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "f", "Red", "Sweet", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "f", "Red", "Sweet", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "nb", "Light Brown", "Grumpy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "nb", "Light Brown", "Grumpy", 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "f", "Black Brown", "Gentle", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "f", "Black Brown", "Gentle", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "nb", "Black", "Grumpy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "nb", "Black", "Grumpy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "f", "White", "Motherly", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "f", "White", "Motherly", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 19, "m", "Blonde", "Rude", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 19, "m", "Blonde", "Rude", 0);
